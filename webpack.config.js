b89c7be3bd18c15d454137725afed8d76556e66f
Rafael Liban <rafaelliban+dev@gmail.com>
2017-03-09 12:22:55 +0800

    add react-hot-loader and webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,59 @@
 /**
  * Created by Rafael on 3/8/2017.
  */
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
 	template: `${__dirname}/app/index.html`,
 	filename: 'index.html',
-	inject: 'body',
+	inject: 'body'
 });
 
 module.exports = {
-	entry: ['./app/index.jsx'],
+	entry: [
+		'react-hot-loader/patch',
+		'webpack-dev-server/client?http://localhost:8080',
+		'webpack/hot/only-dev-server',
+		'./app/index.jsx'
+	],
 	output: {
 		filename: 'index_bundle.js',
 		path: `${__dirname}/dist`,
+		publicPath: '/'
 	},
 	module: {
-		loaders: [
-			{ test: /\.jsx?$/, exclude: /node_modules/, loader: ['babel'] },
-			{ test: /\.s?css$/, loader: 'style!css!sass?outputStyle=compressed' },
+		rules: [
+			{ test: /\.jsx?$/, exclude: /node_modules/, use: ['babel-loader'] },
+			{
+				test: /\.scss$/,
+				use: [
+				{ loader: 'style-loader' },
+				{ loader: 'css-loader' },
+				{
+					loader: 'sass-loader',
+					options: { includePaths: ['./node_modules'], outputStyle: 'compressed' }
+				}
+			] }
 		],
 	},
-	plugins: [HtmlWebpackPluginConfig],
-	sassLoader: {
-		includePaths: ['./node_modules'],
+	devtool: 'inline-source-map',
+	resolve: {
+		modules: [
+			`${__dirname}/app`,
+			'node_modules'
+		],
+		extensions: ['.js', '.jsx', '.css', '.scss', '.json']
 	},
+	plugins: [
+		HtmlWebpackPluginConfig,
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.NamedModulesPlugin()
+	],
+	devServer: {
+		hot: true,
+		contentBase: `${__dirname}/dist`,
+		publicPath: '/'
+	}
 };
