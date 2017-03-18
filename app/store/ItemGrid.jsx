74ac6e397e4c680477f1325609ec45c31d6425d8
Rafael Liban <rafaelliban+dev@gmail.com>
2017-03-19 00:16:21 +0800

    add sorting and display count forms to store view

--- app/store/ItemGrid.jsx
+++ app/store/ItemGrid.jsx
@@ -4,6 +4,9 @@ import Box from 'grommet/components/Box';
 import Label from 'grommet/components/Label';
 import Image from 'grommet/components/Image';
 import Columns from 'grommet/components/Columns';
+import Select from 'grommet/components/Select';
+import Sort from 'grommet-addons/components/Sort';
+import FormField from 'grommet/components/FormField';
 
 const SAMPLE_DATA = [
 	{ image: 'https://placehold.it/200x200', name: 'Sample', description: 'Lorem Ipsum', price: 100, code: 'prod1' },
@@ -22,6 +25,13 @@ const SAMPLE_DATA = [
 	{ image: 'https://placehold.it/200x200', name: 'Sample', description: 'Lorem Ipsum', price: 100, code: 'prod14' },
 	{ image: 'https://placehold.it/200x200', name: 'Sample', description: 'Lorem Ipsum', price: 100, code: 'prod15' }
 ];
+
+const SORT_OPTIONS = [
+	{ label: 'Date Added', value: 'dateAdded' },
+	{ label: 'Price', value: 'price' }
+];
+
+const DISPLAY_OPTIONS = ['20', '40', '60', '80', '100'];
 
 const Item = props => (
 	<Box size="small" pad="small">
@@ -45,9 +55,15 @@ Item.propTypes = {
 };
 
 const ItemGrid = props => (
-	<Columns size="small">
-		{props.items.map(item => <Item key={item.code} {...item} />)}
-	</Columns>
+	<Box flex>
+		<Box direction="row" justify="between">
+			<Sort options={SORT_OPTIONS} onChange={obj => console.log(obj)} />
+			<Select options={DISPLAY_OPTIONS} placeHolder="Display Count" />
+		</Box>
+		<Columns size="small" justify="between">
+			{props.items.map(item => <Item key={item.code} {...item} />)}
+		</Columns>
+	</Box>
 );
 
 ItemGrid.propTypes = {
